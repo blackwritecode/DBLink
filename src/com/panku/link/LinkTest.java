6b7deae89360adc75eaec39771c409f39e06c252
blackwritecode <lisai212@sina.cn>
2016-08-09 09:59:47 +0800

    Update LinkTest.java
    Run code

--- src/com/panku/link/LinkTest.java
+++ src/com/panku/link/LinkTest.java
@@ -1,1 +1,24 @@
 
+package com.panku.link;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class LinkTest {
+
+	public static void main(String[] args) throws SQLException {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://127.0.0.1:6661/mmpbe?allowMultiQueries=true&useUnicode=true&characterEncoding=utf-8";
+			String user = "sa";
+			String password = "sa";
+			Connection con = DriverManager.getConnection(url, user, password);
+			System.out.println(con);
+			con.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
